a2ac4c0d885a4e512b74f2519b4b611a8591651c
Vanessa Drakeford <drakeford@gmx.net>
2022-08-23 10:48:39 +0200

    Created Address Model and View for Adding Data to Form

---
+++ AddressBookApp/Controllers/AddressBookController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBookApp.Controllers
+{
+    public class AddressBookController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ AddressBookApp/Models/AddAddressViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBookApp.Models
+{
+    public class AddAddressViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HomeAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
